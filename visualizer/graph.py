e7cfa9ef7ee577f4020427a2b88bc0e2f70ae79a
Erdem Toruk <erdemtoruk26@gmail.com>
2025-11-12 12:26:41 +0300

    Result graphs

--- visualizer/graph.py
+++ visualizer/graph.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 files = {
     "Insertion Sort": "results/insertion_sort.csv",
@@ -14,6 +15,17 @@ for label, filename in files.items():
     plt.errorbar(df["N"], df["avg"], yerr=[yerr_lower, yerr_upper],
                  fmt='-o', capsize=4, elinewidth=1, label=label)
 
+# --- Theoretical reference curves ---
+#n_values = [i for i in range(100, 5001, 100)]
+#c_nlogn = 1e-5
+#c_n2 = 5e-6
+
+#nlogn_curve = [c_nlogn * n * math.log2(n) for n in n_values]
+#n2_curve = [c_n2 * (n ** 2) for n in n_values]
+
+#plt.plot(n_values, nlogn_curve, 'k--', label=f"{c_nlogn}nlogn (reference)")
+#plt.plot(n_values, n2_curve, 'r--', label=f"{c_n2}n² (reference)")
+
 plt.title("Sorting Algorithm Comparison with Error Bars")
 plt.xlabel("N (Number of Elements)")
 plt.ylabel("Time (ms)")
@@ -21,4 +33,5 @@ plt.yscale("log")
 plt.legend()
 plt.grid(True, linestyle="--", alpha=0.6)
 plt.tight_layout()
+plt.savefig("graphs/filename")
 plt.show()
